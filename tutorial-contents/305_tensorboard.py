402da626b74cafdef9dce0b0ac56413106a18517
EmbraceLife <1227561934@qq.com>
2017-05-27 08:33:01 +0800

    only localhost:6006 in my case

--- tutorial-contents/305_tensorboard.py
+++ tutorial-contents/305_tensorboard.py
@@ -47,3 +47,4 @@ for step in range(100):
 # Lastly, in your terminal or CMD, type this :
 # $ tensorboard --logdir path/to/log
 # open you google chrome, type the link shown on your terminal or CMD. (something like this: http://0.0.0.0:6006)
+# in my case, only use localhost:6006 works
